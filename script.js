cb19db53a7794a91109a952411f9b99a29eb10a2
Lgkleads1 <kibetelkana18@gmail.com>
2025-04-24 17:57:44 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.contact-button').forEach(button => {
+    button.addEventListener('click', function() {
+        alert("You can contact the farmer via email: info@farmfresh.com");
+    });
+});
